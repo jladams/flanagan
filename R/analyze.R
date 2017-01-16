268215ce36496d1e7d81d85c1d08c6a662c56981
jladams <adamsjlp@gmail.com>
2017-01-16 14:41:23 -0500

    commenting and cleaning

--- R/analyze.R
+++ R/analyze.R
@@ -12,6 +12,7 @@ first_line <- function(df, wordCount = 9, diff = 3) {
   
   i <- 1
   
+  # Iterate through each line, compare white space to number of words (higher amount of white space indicates formatting for table of contents, etc.)
   for (line in lines$lines) {
     charsWhite <- stri_stats_latex(line)[3]
     words <- stri_stats_latex(line)[4]
@@ -22,6 +23,7 @@ first_line <- function(df, wordCount = 9, diff = 3) {
     i <- i + 1
   }
   
+  # Return the line detected as the first of the body
   return(lines[i,])
   
 }
@@ -40,19 +42,30 @@ first_word <- function(df) {
 
 # Get middle word and last sentence
 middle_end <- function(df){
-  
+
+  # Tokenize work into individual words  
   words <- df %>%
     unnest_tokens(words, text)
 
+  # Tokenize work into sentences
   sentences <- df %>%
     unnest_tokens(sentences, text, token = "sentences")
 
+  # Run first_word() on work to get the beginning
   beginning <- first_word(df)
+  
+  # Find the word in the middle
   middle <- words[length(words$words)/2, ]
+  
+  # Find the word at the end
   end <- sentences[length(sentences$sentences), ]
   
-  df <- left_join(beginning, middle)
-  df <- left_join(df, end)
+  # Combine beginning, middle, and end
+  df <- beginning %>%
+    left_join(middle) %>%
+    left_join(end)
+  
+  # Rename columns appropriately
   colnames(df) <- c("gutenberg_id", "title", "author", "beginning", "middle", "end")
   
   return(df)
@@ -61,10 +74,12 @@ middle_end <- function(df){
 # Provide a vector of Gutenberg ID numbers to get the first word, middle word, and last sentence
 analyze <- function(work) {
   
+  # Retrieve a list of dataframes containing the text to be analyzed
   dlist <- lapply(work, gutenberg_download, meta_fields = c("title", "author"))
   
+  # Apply the middle_end() function to each and then bind the list together to create a single dataframe of results
   df <- lapply(dlist, middle_end) %>%
-    bind_rows
+    bind_rows()
   
   return(df)
 }
