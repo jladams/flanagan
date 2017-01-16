502ecbb8b2b1e081501eba3b3346985251588876
jladams <adamsjlp@gmail.com>
2017-01-16 14:31:39 -0500

    get gutenberg id numbers. analyze function returns beginning, middle, and end

--- R/analyze.R
+++ R/analyze.R
@@ -2,9 +2,44 @@ library(tidyverse)
 library(gutenbergr)
 library(tidytext)
 library(stringi)
+library(stringr)
 
-middle_end <- function(work){
-  df <- gutenberg_download(work, meta_fields = c("title", "author"))
+# Get first line of work based on word count of line vs. number of blank characters
+first_line <- function(df, wordCount = 9, diff = 3) {
+  
+  lines <- df %>%
+    unnest_tokens(lines, text, token = "lines")
+  
+  i <- 1
+  
+  for (line in lines$lines) {
+    charsWhite <- stri_stats_latex(line)[3]
+    words <- stri_stats_latex(line)[4]
+    if((words >= wordCount) & ((charsWhite - words) <= diff)) {
+      firstLine <- line
+      break
+    }
+    i <- i + 1
+  }
+  
+  return(lines[i,])
+  
+}
+
+
+# Get first word of first line
+first_word <- function(df) {
+  
+  fword <- first_line(df) %>%
+    mutate(beginning = word(lines)) %>%
+    select(-lines)
+
+  return(fword) 
+  
+}
+
+# Get middle word and last sentence
+middle_end <- function(df){
   
   words <- df %>%
     unnest_tokens(words, text)
@@ -12,41 +47,36 @@ middle_end <- function(work){
   sentences <- df %>%
     unnest_tokens(sentences, text, token = "sentences")
 
+  beginning <- first_word(df)
   middle <- words[length(words$words)/2, ]
   end <- sentences[length(sentences$sentences), ]
-
-  df <- left_join(middle, end)
-  colnames(df) <- c("gutenberg_id", "title", "author", "middle", "end")
+  
+  df <- left_join(beginning, middle)
+  df <- left_join(df, end)
+  colnames(df) <- c("gutenberg_id", "title", "author", "beginning", "middle", "end")
   
   return(df)
 }
 
+# Provide a vector of Gutenberg ID numbers to get the first word, middle word, and last sentence
 analyze <- function(work) {
-  df <- lapply(work, middle_end) %>%
+  
+  dlist <- lapply(work, gutenberg_download, meta_fields = c("title", "author"))
+  
+  df <- lapply(dlist, middle_end) %>%
     bind_rows
   
   return(df)
 }
 
+# Get vector of Gutenberg ID numbers by exact subject
+books_by_subject <- function(search) {
+  
+  subs <- gutenberg_subjects %>%
+    filter(subject == search)
 
-first_line <- function(work) {
-  df <- gutenberg_download(work, meta_fields = c("title", "author"))
+  return(subs$gutenberg_id)
   
-  lines <- df %>%
-    unnest_tokens(lines, text, token = "lines")
-  
-  for (line in lines$lines) {
-    charsWhite <- stri_stats_latex(line)[3]
-    words <- stri_stats_latex(line)[4]
-    if((words > 10) & ((charsWhite - words) < 2)) {
-      firstLine <- line
-      break
-    }
-  }
-  
-  return(firstLine)
-
 }
 
 
-records <- analyze(c(42, 57, 5400, 777, 874))
